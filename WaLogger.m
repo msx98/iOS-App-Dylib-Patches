a8598c54b7c1770e3ddbe33fb78f90967fac8296
msx98 <msx98@lx.ax>
2026-02-24 11:31:03 +0200

    add vscode stuff

--- WaLogger.m
+++ WaLogger.m
@@ -1,140 +1,73 @@
 #import <Foundation/Foundation.h>
 #import <objc/runtime.h>
 
-// --- Logger Macro and Helper ---
 #define log_debug(fmt, ...) \
     do { \
         NSString *msg = [NSString stringWithFormat:@"[WaLogger] " fmt, ##__VA_ARGS__]; \
         NSLog(@"%@", msg); \
         write_to_log_file(msg); \
-    } while (0);
+    } while (0)
 
 static void write_to_log_file(NSString *message) {
-    static NSDateFormatter *dateFormatter = nil;
     static NSFileHandle *fileHandle = nil;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         NSString *docPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
         NSString *logPath = [docPath stringByAppendingPathComponent:@"call_intercept_log.txt"];
-        
-        NSFileManager *dfm = [NSFileManager defaultManager];
-        if (![dfm fileExistsAtPath:logPath]) {
-            [dfm createFileAtPath:logPath contents:nil attributes:nil];
+        if (![[NSFileManager defaultManager] fileExistsAtPath:logPath]) {
+            [[NSFileManager defaultManager] createFileAtPath:logPath contents:nil attributes:nil];
         }
         fileHandle = [NSFileHandle fileHandleForWritingAtPath:logPath];
         [fileHandle seekToEndOfFile];
-        dateFormatter = [[NSDateFormatter alloc] init];
-        [dateFormatter setDateFormat:@"yyyy-MM-dd HH:mm:ss.SSS"];
     });
-
     if (fileHandle) {
-        NSString *timestamp = [dateFormatter stringFromDate:[NSDate date]];
-        
-        // Prepare data
-        NSString *logLine = [NSString stringWithFormat:@"[%@] %@\n", timestamp, message];
-        NSData *data = [logLine dataUsingEncoding:NSUTF8StringEncoding];
-        
-        if (data) {
-            [fileHandle writeData:data];
-            [fileHandle synchronizeFile]; // Flush to disk
-        }
+        NSString *logLine = [NSString stringWithFormat:@"%@ %@\n", [NSDate date], message];
+        [fileHandle writeData:[logLine dataUsingEncoding:NSUTF8StringEncoding]];
+        [fileHandle synchronizeFile];
     }
 }
 
-// --- Swizzling Helper ---
-
-static void swizzle_wa(Class cls, SEL originalSelector, SEL swizzledSelector) {
+// Global Swizzler
+static void swizzle_any(Class cls, SEL originalSelector, SEL swizzledSelector) {
     Method originalMethod = class_getInstanceMethod(cls, originalSelector);
     Method swizzledMethod = class_getInstanceMethod(cls, swizzledSelector);
-    
-    if (!originalMethod || !swizzledMethod) {
-        log_debug(@"Error: Missing method for selector %@", NSStringFromSelector(originalSelector));
-        return;
+    if (originalMethod && swizzledMethod) {
+        method_exchangeImplementations(originalMethod, swizzledMethod);
     }
-
-    method_exchangeImplementations(originalMethod, swizzledMethod);
 }
 
-// --- Hook Implementation ---
+@implementation NSObject (Discovery)
 
-@interface NSObject (WACallHooks)
-@end
-
-@implementation NSObject (WACallHooks)
-
-- (void)hook_setLgcParticipantJIDs:(id)arg1 {
-    log_debug(@"setLgcParticipantJIDs: %@", arg1);
-    [self hook_setLgcParticipantJIDs:arg1];
+// Hooking init for ALL objects to find the WACall subclass
+- (instancetype)discovery_init {
+    NSString *className = NSStringFromClass([self class]);
+    if ([className containsString:@"WACall"] || [className containsString:@"CallManager"]) {
+        log_debug(@"INSTANCE CREATED: %@", className);
+    }
+    return [self discovery_init];
 }
 
-- (void)hook_setOfferProcessed:(BOOL)arg1 {
-    log_debug(@"setOfferProcessed: %@", arg1 ? @"YES" : @"NO");
-    [self hook_setOfferProcessed:arg1];
-}
-
-- (void)hook_setCallOfferID:(id)arg1 {
-    log_debug(@"setCallOfferID: %@", arg1);
-    [self hook_setCallOfferID:arg1];
-}
-
-- (void)hook_setIsCallOfferRering:(BOOL)arg1 {
-    log_debug(@"setIsCallOfferRering: %@", arg1 ? @"YES" : @"NO");
-    [self hook_setIsCallOfferRering:arg1];
-}
-
-- (void)hook_setOldCallWaitingType:(NSInteger)arg1 {
-    log_debug(@"setOldCallWaitingType: %ld", (long)arg1);
-    [self hook_setOldCallWaitingType:arg1];
-}
-
-- (void)hook_setEndedTimestamp:(id)arg1 {
-    log_debug(@"setEndedTimestamp: %@", arg1);
-    [self hook_setEndedTimestamp:arg1];
-}
-
-- (void)hook_setCallID:(id)arg1 {
-    log_debug(@"setCallID: %@", arg1);
-    [self hook_setCallID:arg1];
+// Placeholder hooks for the dynamic discovery
+- (void)hook_generic_setter:(id)arg1 {
+    log_debug(@"[%@] SETTER CALLED with: %@", NSStringFromClass([self class]), arg1);
+    [self hook_generic_setter:arg1];
 }
 
 @end
 
-static Class find_wa_class() {
-    Class cls;
-    NSArray *candidates = @[
-        @"WhatsApp.WACall", 
-        @"WACall", 
-        @"_TtC8WhatsApp6WACall", // Common mangled name
-        @"Core.WACall"           // Sometimes moved to a Core module
-    ];
-
-    for (NSString *name in candidates) {
+__attribute__((constructor))
+static void init_discovery() {
+    // 1. Hook NSObject init to find the exact subclass name used during a call
+    swizzle_any([NSObject class], @selector(init), @selector(discovery_init));
+    
+    // 2. Try to hook known candidates immediately
+    NSArray *classes = @[@"WhatsApp.WACall", @"WACall", @"_TtC8WhatsApp6WACall", @"WhatsApp.WAGroupCall"];
+    for (NSString *name in classes) {
         Class cls = NSClassFromString(name);
         if (cls) {
-            log_debug(@"[WaLogger] Success! Found class with name: %@", name);
-            return cls;
+            log_debug(@"Found candidate: %@. Hooking setters...", name);
+            swizzle_any(cls, @selector(setCallID:), @selector(hook_generic_setter:));
+            swizzle_any(cls, @selector(setLgcParticipantJIDs:), @selector(hook_generic_setter:));
         }
-    }
-    return nil;
-}
-
-// --- Constructor ---
-__attribute__((constructor))
-static void init_logger_hooks() {
-    log_debug(@"called init_logger_hooks()")
-    Class waCallClass = find_wa_class();
-    
-    if (waCallClass) {
-        swizzle_wa(waCallClass, @selector(setLgcParticipantJIDs:), @selector(hook_setLgcParticipantJIDs:));
-        swizzle_wa(waCallClass, @selector(setOfferProcessed:), @selector(hook_setOfferProcessed:));
-        swizzle_wa(waCallClass, @selector(setCallOfferID:), @selector(hook_setCallOfferID:));
-        swizzle_wa(waCallClass, @selector(setIsCallOfferRering:), @selector(hook_setIsCallOfferRering:));
-        swizzle_wa(waCallClass, @selector(setOldCallWaitingType:), @selector(hook_setOldCallWaitingType:));
-        swizzle_wa(waCallClass, @selector(setEndedTimestamp:), @selector(hook_setEndedTimestamp:));
-        swizzle_wa(waCallClass, @selector(setCallID:), @selector(hook_setCallID:));
-        
-        log_debug(@"Hooks applied to WhatsApp.WACall successfully.");
-    } else {
-        log_debug(@"[WaLogger] Critical: WhatsApp.WACall class not found.");
     }
 }
