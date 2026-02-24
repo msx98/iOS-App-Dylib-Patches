058017c3ea731c774d23bfde6b31387ae8349f3f
msx98 <msx98@lx.ax>
2026-02-24 04:58:25 +0200

    WAV call recording works

--- CallRecordWav.m
+++ CallRecordWav.m
@@ -1,99 +1,124 @@
 #import <Foundation/Foundation.h>
 #import <AudioToolbox/AudioToolbox.h>
 #import "fishhook.h"
-#define FIX_HEADER_PERIOD 100
 
+#define FIX_HEADER_PERIOD_BYTES (1024 * 100) // Update header every 100KB
+
+// --- Improved Structs ---
+#pragma pack(push, 1)
+typedef struct {
+    char     riff[4];        // "RIFF"
+    uint32_t fileSize;       // Total file size - 8
+    char     wave[4];        // "WAVE"
+    char     fmt[4];         // "fmt "
+    uint32_t fmtLen;         // 16 for PCM
+    uint16_t format;         // 3 = IEEE Float, 1 = PCM
+    uint16_t channels;       // 1 = Mono
+    uint32_t sampleRate;     // e.g., 24000
+    uint32_t byteRate;       // sampleRate * channels * (bitsPerSample/8)
+    uint16_t blockAlign;     // channels * (bitsPerSample/8)
+    uint16_t bitsPerSample;  // 32 for float
+    char     data[4];        // "data"
+    uint32_t dataLen;        // Actual audio data size
+} WavHeader;
+#pragma pack(pop)
+
+typedef struct {
+    __unsafe_unretained NSFileHandle *fileHandle;
+    uint32_t totalDataBytes;
+    uint32_t lastCommittedBytes;
+} WavRecorder;
+
+// Static instances for Mic and Speaker
+static WavRecorder micRecorder;
+static WavRecorder speakerRecorder;
+
+// --- Function Pointers for Hooks ---
 static OSStatus (*orig_AudioUnitRender)(AudioUnit, AudioUnitRenderActionFlags *, const AudioTimeStamp *, UInt32, UInt32, AudioBufferList *);
 static OSStatus (*orig_AudioUnitSetProperty)(AudioUnit, AudioUnitPropertyID, AudioUnitScope, AudioUnitElement, const void *, UInt32);
+static AURenderCallback orig_outputCallback = NULL;
 
-static NSFileHandle *micFile = nil;
-static NSFileHandle *speakerFile = nil;
+// --- Implementation ---
 
-static uint32_t totalMicBytes = 0;
-static uint32_t totalSpeakerBytes = 0;
-static uint32_t totalMicBytesLast = 0;
-static uint32_t totalSpeakerBytesLast = 0;
-
-// --- WAV Header (44 Bytes) ---
-typedef struct {
-    char     riff[4];
-    uint32_t fileSize;
-    char     wave[4];
-    char     fmt[4];
-    uint32_t fmtLen;
-    uint16_t format;       // 3 = IEEE Float
-    uint16_t channels;     // 1 = Mono
-    uint32_t sampleRate;   // 25000 
-    uint32_t byteRate;
-    uint16_t blockAlign;
-    uint16_t bitsPerSample;
-    char     data[4];
-    uint32_t dataLen;
-} WavHeader;
-
-// --- Periodic Header Update ---
-void sealWavHeader(NSFileHandle *file, uint32_t *currentBytes, uint32_t *currentBytesLast) {
-    if (!file) return;
-    @synchronized(file) {
-        unsigned long long currentPos = [file offsetInFile];
-        uint32_t anticipatedData = *currentBytes + (FIX_HEADER_PERIOD * 4096); 
-        uint32_t fileSizeField = anticipatedData + 44 - 8;
-        [file seekToFileOffset:4];
-        [file writeData:[NSData dataWithBytes:&fileSizeField length:4]];
-        [file seekToFileOffset:40];
-        [file writeData:[NSData dataWithBytes:&anticipatedData length:4]];
-        [file seekToFileOffset:currentPos];
-        *currentBytesLast = currentPos;
-    }
+void updateWavHeader(WavRecorder *recorder) {
+    if (!recorder->fileHandle) return;
+    
+    uint32_t dataSize = recorder->totalDataBytes;
+    uint32_t totalFileSize = dataSize + sizeof(WavHeader) - 8;
+    
+    unsigned long long currentPos = [recorder->fileHandle offsetInFile];
+    
+    // Seek to fileSize (offset 4)
+    [recorder->fileHandle seekToFileOffset:4];
+    [recorder->fileHandle writeData:[NSData dataWithBytes:&totalFileSize length:4]];
+    
+    // Seek to dataLen (offset 40)
+    [recorder->fileHandle seekToFileOffset:40];
+    [recorder->fileHandle writeData:[NSData dataWithBytes:&dataSize length:4]];
+    
+    [recorder->fileHandle seekToFileOffset:currentPos];
+    recorder->lastCommittedBytes = dataSize;
+    
+    // Force OS to flush buffers to disk
+    [recorder->fileHandle synchronizeFile];
 }
 
-void writeWavHeader(NSFileHandle *file) {
+void writeInitialHeader(NSFileHandle *file) {
     WavHeader h;
-    memcpy(h.riff, "RIFF", 4); memcpy(h.wave, "WAVE", 4); memcpy(h.fmt, "fmt ", 4);
-    h.fmtLen = 16; h.format = 3; h.channels = 1; h.sampleRate = 25000; h.bitsPerSample = 32;
+    memcpy(h.riff, "RIFF", 4);
+    memcpy(h.wave, "WAVE", 4);
+    memcpy(h.fmt,  "fmt ", 4);
+    
+    h.fmtLen = 16;
+    h.format = 3;           // 3 = IEEE Float (Standard for AudioUnit buffers)
+    h.channels = 1; 
+    h.sampleRate = 24000;   // Ensure this matches your AudioUnit settings
+    h.bitsPerSample = 32;
     h.byteRate = h.sampleRate * h.channels * (h.bitsPerSample / 8);
     h.blockAlign = h.channels * (h.bitsPerSample / 8);
-    memcpy(h.data, "data", 4); h.fileSize = 0xFFFFFFFF; h.dataLen = 0xFFFFFFFF;
+
+    memcpy(h.data, "data", 4);
+    h.fileSize = 0; // Will be updated by updateWavHeader
+    h.dataLen = 0;  // Will be updated by updateWavHeader
+    
     [file writeData:[NSData dataWithBytes:&h length:sizeof(WavHeader)]];
 }
 
-void appendAudio(NSFileHandle *file, uint32_t *totalBytes, uint32_t *totalBytesLast, AudioBufferList *ioData) {
-    for (UInt32 i = 0; i < ioData->mNumberBuffers; i++) {
-        [file writeData:[NSData dataWithBytes:ioData->mBuffers[i].mData length:ioData->mBuffers[i].mDataByteSize]];
-        *totalBytes += ioData->mBuffers[i].mDataByteSize;
-    }
-    if ((*totalBytes) - (*totalBytesLast) > FIX_HEADER_PERIOD) {
-        sealWavHeader(file, totalBytes, totalBytesLast);
+void appendAudio(WavRecorder *recorder, AudioBufferList *ioData) {
+    if (!recorder->fileHandle || !ioData) return;
+    
+    @synchronized(recorder->fileHandle) {
+        for (UInt32 i = 0; i < ioData->mNumberBuffers; i++) {
+            UInt32 len = ioData->mBuffers[i].mDataByteSize;
+            if (len == 0) continue;
+            
+            NSData *data = [NSData dataWithBytesNoCopy:ioData->mBuffers[i].mData length:len freeWhenDone:NO];
+            [recorder->fileHandle writeData:data];
+            recorder->totalDataBytes += len;
+        }
+        
+        // Periodic commit
+        if (recorder->totalDataBytes - recorder->lastCommittedBytes > FIX_HEADER_PERIOD_BYTES) {
+            updateWavHeader(recorder);
+        }
     }
 }
 
-void setupFiles() {
-    NSString *docPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
-    micFile = [NSFileHandle fileHandleForWritingAtPath:[docPath stringByAppendingPathComponent:@"mic.bin"]];
-    speakerFile = [NSFileHandle fileHandleForWritingAtPath:[docPath stringByAppendingPathComponent:@"speaker.bin"]];
-}
+// --- Hooks ---
 
-// Intercept the app's output callback (The Speaker)
-static AURenderCallback orig_outputCallback = NULL;
 OSStatus hooked_outputCallback(void *inRefCon, AudioUnitRenderActionFlags *ioActionFlags, const AudioTimeStamp *inTimeStamp, UInt32 inBusNumber, UInt32 inNumberFrames, AudioBufferList *ioData) {
-    OSStatus status = noErr;
-    if (orig_outputCallback) {
-        status = orig_outputCallback(inRefCon, ioActionFlags, inTimeStamp, inBusNumber, inNumberFrames, ioData);
-    }
-    if (status == noErr && ioData != NULL) {
-        appendAudio(speakerFile, &totalSpeakerBytes, &totalSpeakerBytesLast, ioData);
+    OSStatus status = orig_outputCallback ? orig_outputCallback(inRefCon, ioActionFlags, inTimeStamp, inBusNumber, inNumberFrames, ioData) : noErr;
+    if (status == noErr && ioData) {
+        appendAudio(&speakerRecorder, ioData);
     }
     return status;
 }
 
 OSStatus hooked_AudioUnitRender(AudioUnit inUnit, AudioUnitRenderActionFlags *ioActionFlags, const AudioTimeStamp *inTimeStamp, UInt32 inBusNumber, UInt32 inNumberFrames, AudioBufferList *ioData) {
-    
     OSStatus status = orig_AudioUnitRender(inUnit, ioActionFlags, inTimeStamp, inBusNumber, inNumberFrames, ioData);
-    
-    if (status == noErr && ioData != NULL) {
-        if (inBusNumber == 1) { 
-            appendAudio(micFile, &totalMicBytes, &totalMicBytesLast, ioData);
-        }
+    // Bus 1 is typically the Input/Mic bus
+    if (status == noErr && inBusNumber == 1 && ioData) {
+        appendAudio(&micRecorder, ioData);
     }
     return status;
 }
@@ -104,10 +129,28 @@ OSStatus hooked_AudioUnitSetProperty(AudioUnit inUnit, AudioUnitPropertyID inID,
         if (cbStruct->inputProc != hooked_outputCallback) {
             orig_outputCallback = cbStruct->inputProc;
             cbStruct->inputProc = hooked_outputCallback;
-            NSLog(@"[CallRecord] Speaker Callback Hooked");
         }
     }
     return orig_AudioUnitSetProperty(inUnit, inID, inScope, inElement, inData, inDataSize);
+}
+
+void setupFiles() {
+    NSString *docPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
+    
+    for (NSString *name in @[@"mic.wav", @"speaker.wav"]) {
+        NSString *path = [docPath stringByAppendingPathComponent:name];
+        [[NSFileManager defaultManager] createFileAtPath:path contents:nil attributes:nil];
+        
+        NSFileHandle *handle = [NSFileHandle fileHandleForWritingAtPath:path];
+        if (handle) {
+            writeInitialHeader(handle);
+            if ([name containsString:@"mic"]) {
+                micRecorder.fileHandle = handle;
+            } else {
+                speakerRecorder.fileHandle = handle;
+            }
+        }
+    }
 }
 
 __attribute__((constructor))
